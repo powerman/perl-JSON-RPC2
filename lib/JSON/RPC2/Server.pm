069b4a9b7ac7a5be38e2bd76412e6aa32a8a0202
Alex Efros <powerman@powerman.name>
2016-02-18 10:16:04 +0200

    v2.1.1

--- lib/JSON/RPC2/Server.pm
+++ lib/JSON/RPC2/Server.pm
@@ -5,7 +5,7 @@ use strict;
 use utf8;
 use Carp;
 
-our $VERSION = 'v2.1.0';
+our $VERSION = 'v2.1.1';
 
 use JSON::MaybeXS;
 
@@ -204,7 +204,7 @@ JSON::RPC2::Server - Transport-independent JSON-RPC 2.0 server
 
 =head1 VERSION
 
-This document describes JSON::RPC2::Server version v2.1.0
+This document describes JSON::RPC2::Server version v2.1.1
 
 
 =head1 SYNOPSIS
